ec16cee7568204f10d043c46da615aad2a2dd5c0
Diana-Victores <dcpernillo@gmail.com>
2020-06-08 14:31:23 -0600

    conexion

--- 18/src/modelo/Conexion.java
+++ 18/src/modelo/Conexion.java
@@ -8,7 +8,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Conexion {
-    
+   
     
     private final String base = "tienda";
     private final String user = "root";
